2fc299ec3e4ff81208bf7449790ee91a291bc641
gadini <alvesrochagadini@gmail.com>
2025-07-05 00:34:21 -0300

    Criando medico request

---
+++ medical.software/src/main/java/com/github/gadini/medical/software/domain/request/DadosMedicoRequest.java
@@ -0,0 +1,52 @@
+package com.github.gadini.medical.software.domain.request;
+
+import com.github.gadini.medical.software.persistence.entity.Endereco;
+
+public class DadosMedicoRequest {
+
+    private String nome;
+    private String email;
+    private String crm;
+    private String telefone;
+    private Endereco endereco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
