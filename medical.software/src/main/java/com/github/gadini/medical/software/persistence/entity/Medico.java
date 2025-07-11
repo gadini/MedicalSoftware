bd6bdfd35045e2e17a67de93a785aca26f937b3a
gadini <alvesrochagadini@gmail.com>
2025-07-10 22:35:19 -0300

    Adicionando propriedade name no @Entity para padronizar anotacoes de entidades

--- medical.software/src/main/java/com/github/gadini/medical/software/persistence/entity/Medico.java
+++ medical.software/src/main/java/com/github/gadini/medical/software/persistence/entity/Medico.java
@@ -7,7 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-@Entity
+@Entity(name = "Medico")
 @Table(name = "medicos")
 public class Medico {
 
