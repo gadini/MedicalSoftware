49cba50ddfd3ad2590fe7238e849105b9e4f7150
gadini <alvesrochagadini@gmail.com>
2025-07-10 23:38:28 -0300

    Adicionando metodo findByLogin no UsuarioRepository

--- medical.software/src/main/java/com/github/gadini/medical/software/persistence/repository/UsuarioRepository.java
+++ medical.software/src/main/java/com/github/gadini/medical/software/persistence/repository/UsuarioRepository.java
@@ -2,6 +2,8 @@ package com.github.gadini.medical.software.persistence.repository;
 
 import com.github.gadini.medical.software.persistence.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByLogin(String login);
 }
