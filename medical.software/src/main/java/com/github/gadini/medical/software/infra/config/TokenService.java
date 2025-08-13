2cf065a16c1b5a8e47df00c0426ce9dcb0900796
gustavo.gadini <gustavo.gadini@redeflex.com.br>
2025-08-13 09:55:21 -0300

    Adicionando autenticacao e autorizacao

--- medical.software/src/main/java/com/github/gadini/medical/software/infra/config/TokenService.java
+++ medical.software/src/main/java/com/github/gadini/medical/software/infra/config/TokenService.java
@@ -3,6 +3,7 @@ package com.github.gadini.medical.software.infra.config;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.github.gadini.medical.software.persistence.entity.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -28,6 +29,19 @@ public class TokenService {
                     .sign(algorithm);
         } catch (JWTCreationException exception){
             throw new RuntimeException("Erro ao gerar token jwt", exception);
+        }
+    }
+
+    public String getSubject(String tokenJwt){
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("medical.software")
+                    .build()
+                    .verify(tokenJwt)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
         }
     }
 
---
+++ medical.software/src/main/java/com/github/gadini/medical/software/infra/filters/SecurityFilter.java
@@ -0,0 +1,54 @@
+package com.github.gadini.medical.software.infra.filters;
+
+import com.github.gadini.medical.software.infra.config.TokenService;
+import com.github.gadini.medical.software.persistence.repository.UsuarioRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String tokenJwt = recoverToken(request);
+
+        if(tokenJwt != null){
+            String subject = tokenService.getSubject(tokenJwt);
+            UserDetails userDetails = usuarioRepository.findByLogin(subject);
+
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+
+        String authorization = request.getHeader("Authorization");
+
+        if(authorization != null){
+            return authorization.replace("Bearer ", "");
+        }
+
+        return null;
+    }
+
+}
